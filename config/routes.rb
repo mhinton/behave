c15f6e9ff111f84296c93896f76e4662e859391c
Matthew Hinton <mhinton@gmail.com>
2011-11-09 14:45:19 -0600

    created home controller and pointed root route to home#index

--- config/routes.rb
+++ config/routes.rb
@@ -53,7 +53,7 @@ Behave::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-  root :to => 'testers#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
