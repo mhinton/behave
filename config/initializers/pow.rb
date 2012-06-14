2ad8dafb8ed79398d06cf976f6d4bc9543cb7bfe
Matthew Hinton <mhinton@gmail.com>
2012-06-14 09:57:00 -0500

    Add initializer for using the POW server on Mac OS X

---
+++ config/initializers/pow.rb
@@ -0,0 +1,12 @@
+# this is for developing using 37Signals POW server on Mac OS X
+# access the debugger via $rdebug -c
+# if (Rails.env.development? || Rails.env.test?) && !$rails_rake_task
+  # require 'debugger'
+
+  # Debugger.settings[:autoeval] = true
+  # Debugger.settings[:autolist] = 1
+  # Debugger.settings[:reload_source_on_change] = true
+  # Debugger.settings[:autoeval] = true
+  # Debugger.start_remote
+  # puts "=> Debugger enabled"
+# end
